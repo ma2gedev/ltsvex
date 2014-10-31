cbf47a45d20d71b54101b9f82d69c8d133d722d9
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-10-31 20:47:23 +0900

    support atomize key for LTSV.dump
    closed #5

--- lib/ltsv.ex
+++ lib/ltsv.ex
@@ -23,11 +23,14 @@ defmodule LTSV do
   iex> LTSV.dump [%{"address" => "Seattle", "name" => "taka"}, %{"address" => "Chicago", "name" => "tetsuo"}]
   "address:Seattle\tname:taka\naddress:Chicago\tname:tetsuo"
 
+  iex> LTSV.dump [%{name: "taka"}]
+  "name:taka"
   """
   def dump(values) do
     Enum.map_join(values, "\n", fn(record) ->
       Enum.map_join(record, "\t", fn(field) ->
         {label, value} = field
+        if is_atom(label), do: label = Atom.to_string(label)
         label <> ":" <> value
       end)
     end)
